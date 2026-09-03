9b90b599d5ff08c9707ef1938011ec8524d3d445
Neh2l <nehalreda14@gmail.com>
2026-09-04 00:28:34 +0300

    edit

--- task02/miniEcommerce/src/layout/Layout.jsx
+++ task02/miniEcommerce/src/layout/Layout.jsx
@@ -8,28 +8,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 export default function Layout() {
-  const [showAnnouncement, setShowAnnouncement] =
-    useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowAnnouncement(false);
-    }, 4000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
 
   return (
     <>
       <Navbar />
 
-      {showAnnouncement && (
-        <div className="announcement">
-          Welcome to Mini Store! 🛍️
-        </div>
-      )}
+     
 
       <main>
         <Outlet />
